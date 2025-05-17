0bcad80aaea79e294bb59bde09a85a5c87a65601
GeorgeSan97 <133137151+GeorgeSan97@users.noreply.github.com>
2025-05-16 19:16:07 -0500

    Generar el DTO Desde el body de la request

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { RegisterUserDto } from "../../domain";
 
 export class AuthController{
     //aquí sí aplicaré inyección de dependencias}
@@ -7,10 +8,13 @@ export class AuthController{
     //controladores de la rut
     //register
     registerUser = (req: Request, res: Response ) => {
-        res.json('registerUser controller');
+        const [error, registerUserDto] = RegisterUserDto.create(req.body);
+        if( error )  res.status(400).json({error});
+
+        res.json(registerUserDto);
     }
 
-    
+
     //login
     loginUser = (req: Request, res: Response ) => {
         res.json('loginUser controller');
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -19,6 +19,9 @@ export class Server {
     }
 
     async start() {
+        //definir middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
 
         this.app.use(this.routes);
         this.app.listen(this.port, () => {
