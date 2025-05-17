44292a0d683966aa2ba854ec4c7b6881c06c500a
GeorgeSan97 <133137151+GeorgeSan97@users.noreply.github.com>
2025-05-16 21:13:37 -0500

    Consumir el repositorio directamente, Sección 2 Terminada

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express"
-import { RegisterUserDto } from "../../domain";
+import { AuthRepository, RegisterUserDto } from "../../domain";
 
 export class AuthController{
     //aquí sí aplicaré inyección de dependencias}
-    constructor() {}
+    constructor(
+        private readonly authRepository: AuthRepository,
+    ) {}
     
     //controladores de la rut
     //register
@@ -11,7 +13,9 @@ export class AuthController{
         const [error, registerUserDto] = RegisterUserDto.create(req.body);
         if( error )  res.status(400).json({error});
 
-        res.json(registerUserDto);
+        this.authRepository.register(registerUserDto!)
+            .then( user => res.json(user) )
+            .catch( error => res.status(400).json({error}) );
     }
 
 
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
+import { AuthDatasourceImpl, AuthRepositoryImpl } from '../../infrastructure';
 
 export class AuthRoutes{
 
@@ -7,7 +8,11 @@ export class AuthRoutes{
     static get routes():Router{
         const router = Router();
 
-        const controller = new AuthController();
+        const datasource = new AuthDatasourceImpl();
+
+        const authRepository = new AuthRepositoryImpl(datasource);
+
+        const controller = new AuthController(authRepository);
 
         //aquí voy a definir mis rutas principales
         router.use('/login',(req,resp) => controller.loginUser(req,resp));
